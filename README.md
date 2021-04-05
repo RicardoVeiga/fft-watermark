0731d82e749ebe4e3bb7eb88ce833ec71492d08b
Ricardo Veiga <ricardojorge@martinsveiga.com>
2021-04-05 23:08:38 +0100

    added a progress bar and a gif generator

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 fft-watermark
 =============
 
-The code for the post ["The Two-Dimensional Fourier Transform and Digital Watermarking"](http://jeremykun.com/2013/12/30/the-two-dimensional-fourier-transform-and-digital-watermarking/)
+Fork from the code for the post ["The Two-Dimensional Fourier Transform and Digital Watermarking"](http://jeremykun.com/2013/12/30/the-two-dimensional-fourier-transform-and-digital-watermarking/)
+
+Motivation for this fork came from this post: https://www.reddit.com/r/opencv/comments/meym5d/question_is_there_a_way_to_replicate_this_fourier/
+
+Updated to Python 3.8.
+
+Added a progressbar and an automatic gif generator. [not optimal results, work in progress]
--- imagetest.py
+++ imagetest.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import imageio
+import os
 from pathlib import Path
+from moviepy.editor import ImageSequenceClip
+from tqdm import tqdm
 
 current_path = Path(__file__).parent.absolute()
 
@@ -45,6 +48,47 @@ def sortedIndices(A):
    return np.array(indexList)
 
 
+def gif(filename, array, fps=1000, scale=1.0):
+    """Creates a gif given a stack of images using moviepy
+
+    Code from: https://gist.github.com/nirum/d4224ad3cd0d71bfef6eba8f3d6ffd59
+    Credit to Niru Maheswaranatha
+    
+    Notes
+    -----
+    works with current Github version of moviepy (not the pip version)
+    https://github.com/Zulko/moviepy/commit/d4c9c37bc88261d8ed8b5d9b7c317d13b2cdf62e
+    Usage
+    -----
+    >>> X = randn(100, 64, 64)
+    >>> gif('test.gif', X)
+    Parameters
+    ----------
+    filename : string
+        The filename of the gif to write to
+    array : array_like
+        A numpy array that contains a sequence of images
+    fps : int
+        frames per second (default: 10)
+    scale : float
+        how much to rescale each image by (default: 1.0)
+    """
+
+    # ensure that the file has the .gif extension
+    fname, _ = os.path.splitext(filename)
+    filename = fname + '.gif'
+
+    # copy into the color dimension if the images are black and white
+    if array.ndim == 3:
+        array = array[..., np.newaxis] * np.ones(3)
+
+    # make the moviepy clip
+    clip = ImageSequenceClip(list(array), fps=fps).resize(scale)
+    clip.write_gif(filename, fps=fps, program='ffmpeg', opt='optimizeplus')
+    return clip
+
+
+
 def animation(filename):
    A = imageio.imread(filename, as_gray=True)
 
@@ -61,8 +105,15 @@ def animation(filename):
    Path("{}/frames".format(current_path)).mkdir(parents=True, exist_ok=True)
    Path("{}/waves".format(current_path)).mkdir(parents=True, exist_ok=True)
 
+   frames = []
+   waves = []
+   waves_and_frames = []
+
    # only process every other index because every frequency has the
    # same magnitude as its symmetric opposite about the origin.
+
+   pbar = tqdm(total=len(decreasingIndices[::2]))
+
    for i,j in decreasingIndices[::2]:
       wave = np.zeros(A.shape)
 
@@ -72,17 +123,28 @@ def animation(filename):
 
       ifftFrame = np.fft.ifft2(np.copy(frame))
       ifftFrame = [[x.real for x in row] for row in ifftFrame]
-      imageio.imwrite('frames/%06d.png' % t, np.array(ifftFrame, dtype=np.uint8))
+      # imageio.imwrite('frames/%06d.png' % t, np.array(ifftFrame, dtype=np.uint8))
+      frames.append(np.array(ifftFrame, dtype=np.uint8))
 
       ifftWave = np.fft.ifft2(np.copy(wave))
       ifftWave = [[x.real for x in row] for row in ifftWave]
-      imageio.imwrite('waves/%06d.png' % t, np.array(ifftWave, dtype=np.uint8))
+      # imageio.imwrite('waves/%06d.png' % t, np.array(ifftWave, dtype=np.uint8))
+      waves.append(np.array(ifftWave, dtype=np.uint8))
+
+      waves_and_frames.append(np.hstack((np.array(ifftWave, dtype=np.uint8), np.array(ifftFrame, dtype=np.uint8))))
 
       t += 1
 
+      pbar.update(1)
+   
+   pbar.close()
+
+   gif('images/frames', np.array(frames))
+   gif('images/waves', np.array(waves))
+
+   gif('images/waves_and_frames', np.array(waves_and_frames))
+
 
 #ifftExample()
-fourierSpectrumExample('images/sherlock.jpg')
+# fourierSpectrumExample('images/sherlock.jpg')
 animation('images/hance-up-sw.png')
-
-
